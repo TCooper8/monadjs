1a80ad52c04116cfab2a8b2ee3dc743c099b501f
Tyler Cooper <coopetyl@live.com>
2016-03-10 18:11:32 -0700

    Updated match statement

--- src/match/index.js
+++ src/match/index.js
@@ -1,37 +1,7 @@
 'use strict'
 
 const Option = require('../option')
-
-let failwith = msg => {
-  throw new Error(msg)
-}
-
-let typePredicate = _type => {
-  switch (_type) {
-  case String:
-  case Object:
-  case Number:
-    return val => {
-      if (val === undefined) {
-        return Option.none
-      }
-      else if (val.constructor === _type) {
-        return Option(val)
-      }
-      else {
-        return Option.none
-      }
-    }
-
-  default:
-    failwith('Type cannot be resolved')
-  }
-}
-
-function Match (onSome, onNone) {
-  this.onSome = onSome
-  this.onNone = onNone
-}
+const Core = require('../core')
 
 function Match() {
   let ruleChain = []
@@ -57,8 +27,26 @@ function Match() {
     failwith('Unable to match value to expression')
   }
 
+  self.equals = expect => {
+    let rule = value => {
+      return expect === value ?
+        Option.some(expect) :
+        Option.none
+    }
+
+    return mapping => {
+      ruleChain.push(rule)
+      methodChain.push(mapping)
+
+      return self
+    }
+  }
+
   self.type = _type => {
-    let predicate = typePredicate(_type)
+    let predicate = value => {
+      return typecheck(_type)(value) ?
+        Option.some(value) : Option.none
+    }
 
     return mapping => {
       ruleChain.push(predicate)
@@ -114,30 +102,4 @@ function Match() {
   return self
 }
 
-let match = () => new Match()
-
-let f = match()
-  .type(String)(filename => {
-    console.log(filename)
-  })
-  .type(Number)(i => f('' + i))
-  .get('name')(name => f(name))
-  .has('name')(obj => f(obj.name))
-
-
-let g = match()
-  .has('name')(match()
-    .type(Object)(match()
-      .get('name')(match()
-        .type(String)(name => {
-          console.log('name = %s', name)
-        })
-        .type(Number)(id => g({ name: '' + id }))
-      )
-    )
-    .any(() => failwith('Expected Object'))
-  )
-  .any(() => failwith('Expected object to contain name.'))
-
-
-g({ name: 'bob' })
+module.exports = () => new Match()
