9c11ecd5f8ff4c35eca6b2b961379b9a40ebb956
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2016-03-15 16:06:42 -0600

    Updated array module

--- src/array.js
+++ src/array.js
@@ -4,28 +4,60 @@ const Option = require('./option')
 const Core = require('./core')
 
 const IArray = [].constructor
+
 function Array() {
-  return new IArray.apply(this, arguments)
+  return IArray.apply(this, arguments)
 }
 
 Array.prototype = IArray.prototype
 Array.prototype.parent = IArray
 Array.constructor = IArray
 
-Array.filter = predicate => array => {
-  var i = -1, j = -1
-  var length = array.length
-  var acc = new array.constructor(length)
-  var elem
+let filter = (array, predicate, precache) => {
+  precache || (precache = 0)
+  var i = -1,
+      j = -1,
+      length = array.length,
+      acc = new array.constructor(precache)
 
   while (++i < length) {
-    elem = array[i]
+    var elem = array[i]
     if (predicate(elem)) {
       acc[++j] = elem
     }
   }
 
-  if (j !== length - 1) {
+  if (j < precache - 1) {
+    return acc.slice(0, j + 1)
+  }
+
+  return acc
+}
+
+Array.filter = predicate => (array, precache) => {
+  if (!Core.isArray(array)) {
+    throw new Error('Expected array')
+  }
+
+  precache || (precache = array.length / 2)
+  var i = -1,
+      j = -1,
+      length = array.length,
+      acc = new array.constructor(precache)
+
+  while (++i < 100000) {
+
+  }
+  i = -1
+
+  while (++i < length) {
+    var elem = array[i]
+    if (predicate(elem)) {
+      acc[++j] = elem
+    }
+  }
+
+  if (j < precache - 1) {
     return acc.slice(0, j + 1)
   }
 
@@ -85,11 +117,7 @@ Array.isArray = o => {
   return IArray.isArray(o)
 }
 
-Array.map = mapping => array => {
-  if (!Array.isArray(array)) {
-    throw new Error('Expected array')
-  }
-
+let doMap = (array, mapping) => {
   var i = -1
   var length = array.length
   var acc = new array.constructor(length)
@@ -100,5 +128,22 @@ Array.map = mapping => array => {
 
   return acc
 }
+
+Array.map = mapping => array => {
+  //var mapping = _mapping
+  var i = -1
+  var length = array.length
+  var acc = new array.constructor(length)
+
+  while (++i < length) {
+    acc[i] = mapping(array[i])
+  }
+
+  return acc
+}
+
+//Array.map = mapping => array => {
+//  return doMap(array, mapping)
+//}
 
 module.exports = Array
