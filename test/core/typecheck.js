bdb203cc0851f070a6aa915fec95a42dd6e3b5d4
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2016-03-15 16:08:14 -0600

    Added test/core typechecking tests

---
+++ test/core/typecheck.js
@@ -0,0 +1,148 @@
+'use strict'
+
+const assert = require('assert')
+const _ = require('../../src')
+
+const Core = _.Core
+const Array = _.Array
+
+let primitives = {
+  'Array': [
+    [],
+    [ 1, 2, 3 ]
+  ],
+  '_Array': [
+    [],
+    [ 1, 2, 3 ]
+  ],
+  'Boolean': [
+    true, false
+  ],
+  'Buffer': [
+    new Buffer(5)
+  ],
+  'Date': [
+    new Date()
+  ],
+  'Error': [
+    new Error()
+  ],
+  'Function': [
+    () => 5,
+    function () { }
+  ],
+  'Map': [
+    new Map()
+  ],
+  'NotObject': [
+    new (function F() { })
+  ],
+  'null': [
+    null
+  ],
+  'Number': [
+    1,
+    5.5
+  ],
+  'Object': [
+    {},
+    { name: 'bob' }
+  ],
+  'RegExp': [
+    new RegExp('j'),
+    /j/g
+  ],
+  'Set': [
+    new Set()
+  ],
+  'String': [
+    'hello'
+  ],
+  'undefined': [
+    undefined
+  ],
+  'WeakMap': [
+    new WeakMap()
+  ],
+  'WeakSet': [
+    new WeakSet()
+  ]
+}
+
+let typeFns = {
+  'Array': [].constructor,
+  '_Array': Array,
+  'Boolean': Boolean,
+  'Buffer': Buffer,
+  'Date': Date,
+  'Error': Error,
+  'Function': Function,
+  'Map': Map,
+  'null': null,
+  'Number': Number,
+  'Object': Object,
+  'RegExp': RegExp,
+  'Set': Set,
+  'String': String,
+  'undefined': undefined,
+  'WeakMap': WeakMap,
+  'WeakSet': WeakSet
+}
+
+let check = (typeKey, unions) => {
+  unions || (unions = [])
+
+  let keys = Object.keys(primitives)
+
+  let type = typeFns[typeKey]
+
+  if (typeKey === 'null') {
+    assert(type === null, 'Expected type mapping to be null')
+  }
+  else if (typeKey === 'undefined') {
+    assert(type === undefined, 'Expected type mapping to be undefined')
+  }
+  else {
+    assert(!!type, sprintf('Type mapping in test is not defined for %s')(typeKey))
+  }
+
+  describe(sprintf('Testing type %s.')(typeKey), () => {
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i]
+      let values = primitives[key]
+
+      if (key === typeKey || unions.find(k => k === key)) {
+        _.map(Array)(val => {
+          it(sprintf('Should typecheck true for %s from %j')(typeKey, val), () => {
+            assert(typecheck(type)(val), sprintf('Type %s should have been true for %j')(typeKey, val))
+          })
+        })(values)
+      }
+      else {
+        _.map(Array)(val => {
+          it(sprintf('Should typecheck false for %s from %j')(typeKey, val), () => {
+            assert(typecheck(type)(val) === false)
+          })
+        })(values)
+      }
+    }
+  })
+}
+
+check('Array', [ '_Array' ])
+check('_Array', [ 'Array' ])
+check('Boolean')
+check('Buffer')
+check('Date')
+check('Error')
+check('Function')
+check('Map')
+check('null')
+check('Number')
+check('Object')
+check('RegExp')
+check('Set')
+check('String')
+check('undefined')
+check('WeakMap')
+check('WeakSet')
